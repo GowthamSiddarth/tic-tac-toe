3ada1741e72069096f7ec203ac0c53aee330933b
Gowtham Behara <gothsiddu@gmail.com>
2019-12-30 22:35:13 +0530

    Create separate Landing component and render it based on route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import './App.css';
-
-import { Container, Row, Col, Button } from "react-bootstrap";
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Container, Row, Col } from "react-bootstrap";
+
+import Landing from "./components/Landing";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Container>
-          <Row>
-            <Col>
-              <Button variant="primary" size="lg">
-                Play Game
-              </Button>
-            </Col>
-          </Row>
-        </Container>
+        <Router>
+          <Container>
+            <Row>
+              <Col>
+                <Route exact path="/" component={Landing} />
+              </Col>
+            </Row>
+          </Container>
+        </Router>
       </header>
     </div>
   );
---
+++ src/components/Landing.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import { Button } from 'react-bootstrap';
+
+class Landing extends Component {
+    render() {
+        return (
+            <Button variant="primary" size="lg">
+                Play Game
+            </Button>
+        );
+    }
+}
+
+export default Landing;
