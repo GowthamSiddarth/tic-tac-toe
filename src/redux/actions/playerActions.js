5aa1cba74e7e211cf1acecd948f69dd2b8f89cd7
Gowtham Behara <gothsiddu@gmail.com>
2019-12-31 11:06:29 +0530

    Create actions for create player

---
+++ src/redux/actions/playerActions.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import { ERROR, CREATE_PLAYER } from "./types";
+
+export const createPlayer = (playerName) => dispatch => {
+    axios.get('/api/v1/v0/create-player/' + playerName)
+    .then(resp => dispatch({
+        type: CREATE_PLAYER,
+        payload: resp.data.message
+    }))
+    .catch(err => dispatch({
+        type: ERROR,
+        payload: err.response.data
+    }));
+}
---
+++ src/redux/actions/types.js
@@ -0,0 +1,3 @@
+export const ERROR = "ERROR"
+
+export const CREATE_PLAYER = "CREATE_PLAYER";
