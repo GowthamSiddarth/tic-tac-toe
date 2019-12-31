7c5d83d4632350ebb3406933e7be94c77570af6b
Gowtham Behara <gothsiddu@gmail.com>
2019-12-31 11:32:19 +0530

    Include provider and store

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { Provider } from "react-redux";
+import store from './redux/store';
+
 import { Container, Row, Col } from "react-bootstrap";
 
 import Landing from "./components/Landing";
@@ -13,15 +16,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Router>
-          <Container>
-            <Row>
-              <Col>
-                <Route exact path="/" component={Landing} />
-              </Col>
-            </Row>
-          </Container>
-        </Router>
+        <Provider store={store}>
+          <Router>
+            <Container>
+              <Row>
+                <Col>
+                  <Route exact path="/" component={Landing} />
+                </Col>
+              </Row>
+            </Container>
+          </Router>
+        </Provider>
       </header>
     </div>
   );
