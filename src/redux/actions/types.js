78b1f302d8ab1515ffda5cad03a5ee2d2c8bee52
Gowtham Behara <gothsiddu@gmail.com>
2020-01-03 14:49:38 +0530

    Remove action type for JOIN_GAME

--- src/redux/actions/types.js
+++ src/redux/actions/types.js
@@ -2,5 +2,5 @@ export const ERROR = "ERROR";
 
 export const CREATE_PLAYER = "CREATE_PLAYER";
 export const CREATE_GAME_ROOM = "CREATE_GAME_ROOM";
-export const JOIN_GAME_ROOM = "JOIN_GAME_ROOM";
+// export const JOIN_GAME_ROOM = "JOIN_GAME_ROOM";
 export const START_NEW_GAME = "START_NEW_GAME";
