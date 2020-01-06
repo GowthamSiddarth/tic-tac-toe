64eae3cd6622d3a10268e2e4a3eb46f580fd578d
Gowtham Behara <gothsiddu@gmail.com>
2020-01-06 13:58:41 +0530

    Create playboard for tic-tac-toe

--- src/components/PlayGame.js
+++ src/components/PlayGame.js
@@ -1,8 +1,39 @@
 import React from "react";
+import "../App.css";
+
+import { Button, Container, Row, Col } from "react-bootstrap";
+
+function Square(props) {
+    return (
+        <Button block={true} variant="outline-light" size="lg" style={{ height: "68px", width: "68px" }}>X</Button>
+    );
+}
+
+function Board(props) {
+    return (
+        <Container>
+            <Row className="justify-content-md-center no-gutters">
+                <Col md="auto"><Square /></Col>
+                <Col md="auto"><Square /></Col>
+                <Col md="auto"><Square /></Col>
+            </Row>
+            <Row className="justify-content-md-center no-gutters">
+                <Col md="auto"><Square /></Col>
+                <Col md="auto"><Square /></Col>
+                <Col md="auto"><Square /></Col>
+            </Row>
+            <Row className="justify-content-md-center no-gutters">
+                <Col md="auto"><Square /></Col>
+                <Col md="auto"><Square /></Col>
+                <Col md="auto"><Square /></Col>
+            </Row>
+        </Container>
+    );
+}
 
 function PlayGame(props) {
     return (
-        <div>Play Game</div>
+        <Board />
     );
 }
 
