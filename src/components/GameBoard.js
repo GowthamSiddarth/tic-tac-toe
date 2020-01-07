3a032ea6f88d65ffc7a2d7aed2b858940570865b
Gowtham Behara <gothsiddu@gmail.com>
2020-01-07 11:12:08 +0530

    Create Grid for game board

---
+++ src/components/GameBoard.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "../App.css";
+
+import { Button, Container, Row, Col } from "react-bootstrap";
+
+function Square(props) {
+    return (
+        <Button block={true} variant="outline-light" size="lg" style={{ height: "68px", width: "68px" }}>X</Button>
+    );
+}
+
+function Board(props) {
+    return (
+        <Container>
+            <Row className="justify-content-md-center no-gutters">
+                <Col md="auto"><Square /></Col>
+                <Col md="auto"><Square /></Col>
+                <Col md="auto"><Square /></Col>
+            </Row>
+            <Row className="justify-content-md-center no-gutters">
+                <Col md="auto"><Square /></Col>
+                <Col md="auto"><Square /></Col>
+                <Col md="auto"><Square /></Col>
+            </Row>
+            <Row className="justify-content-md-center no-gutters">
+                <Col md="auto"><Square /></Col>
+                <Col md="auto"><Square /></Col>
+                <Col md="auto"><Square /></Col>
+            </Row>
+        </Container>
+    );
+}
+
+function GameBoard(props) {
+    return (
+        <Board />
+    );
+}
+
+export default GameBoard;
