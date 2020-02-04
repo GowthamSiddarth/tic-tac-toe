a605a7f37a8b12a03770544f0bc934ba82ef5c03
Gowtham Behara <gothsiddu@gmail.com>
2020-02-04 18:05:39 +0530

    Add action type for quitGame

--- src/redux/actions/types.js
+++ src/redux/actions/types.js
@@ -4,6 +4,7 @@ export const CREATE_PLAYER = "CREATE_PLAYER";
 export const CREATE_GAME_ROOM = "CREATE_GAME_ROOM";
 export const JOIN_GAME_ROOM = "JOIN_GAME_ROOM";
 export const START_NEW_GAME = "START_NEW_GAME";
+export const QUIT_GAME = "QUIT_GAME";
 export const IS_MY_TURN = "IS_MY_TURN";
 export const MAKE_A_MOVE = "MAKE_A_MOVE";
 export const CLEAR_ERROR_MESSAGE = "CLEAR_ERROR_MESSAGE";
